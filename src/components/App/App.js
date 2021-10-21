931cf1cb493548e89228e887849a936a0112635a
Paweł Król <krol.pawel89@gmail.com>
2021-10-21 16:49:27 +0200

    add propTypes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
 
+
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world</h2>
-        <List />
+        <List title={['Things to do ', <sup key='1'>soon!</sup>]}>
+          {/* <p>I'm planning on doing all these things sooner, rather than later!</p> */}
+        </List>
       </main>
     )
   }
