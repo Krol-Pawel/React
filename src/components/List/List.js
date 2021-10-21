ed98fd69fd4e645b797f2e5877260534be391839
Paweł Król <krol.pawel89@gmail.com>
2021-10-21 17:59:27 +0200

    add column.js and change placa for image

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
 import PropTypes from 'prop-types';
+
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
     children: PropTypes.node,
+    image: PropTypes.node,
   };
 
   static defaultProps = {
@@ -16,9 +19,14 @@ class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} />
+        <Hero titleText={this.props.title} imageTwo={this.props.image}/>
         <div className={styles.description}>
           {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column titleColumn={'Animals'}/>
+          <Column titleColumn={'Plants'}/>
+          <Column titleColumn={'Minerals'}/>
         </div>
       </section>
     )
