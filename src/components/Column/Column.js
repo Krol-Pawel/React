07f6e771d998f79a7acef3e08c1372a0defedebc
Paweł Król <krol.pawel89@gmail.com>
2021-10-27 17:32:24 +0200

    modify with reduks

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,13 +3,13 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 import {settings} from '../../data/dataStore';
-import Creator from '../Creator/Creator.js';
+// import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-  state = {
-    cards: this.props.cards || [],
-  }
+  // state = {
+  //   cards: this.props.cards || [],
+  // }
 
   static propTypes = {
     title: PropTypes.string,
@@ -17,21 +17,22 @@ class Column extends React.Component {
     name: PropTypes.string,
   }
 
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-            title,
-          }
-        ]
-      }
-    ));
-  }
+  // addCard(title){
+  //   this.setState(state => (
+  //     {
+  //       cards: [
+  //         ...state.cards,
+  //         {
+  //           key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+  //           title,
+  //         }
+  //       ]
+  //     }
+  //   ));
+  // }
 
   render() {
+    const {title, icon, cards} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{this.props.title}
@@ -40,13 +41,15 @@ class Column extends React.Component {
           </span>
         </h3>
         <div className={styles.cards}>
-          {this.state.cards.map(({key, ...cardsProps}) => (
-            <Card key={key} {...cardsProps} />
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
           ))}
         </div>
+        {/*
         <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
         </div>
+          */}
       </section>
     )
   }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,13 +7,13 @@ import ReactHtmlParser from 'react-html-parser';
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
-    <img className={styles.image} src={props.imageTwo}></img>
+    <img className={styles.image} src={props.image}></img>
   </header>
 );
 
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,
-  imageTwo: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default Hero;
