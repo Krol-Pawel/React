11831a44b5db397dc134c98a0d811a144e0c7356
Paweł Król <krol.pawel89@gmail.com>
2021-11-02 14:49:15 +0100

    modify import of withRouter

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon';
 import Container from '../Container/Container';
-import { withRouter } from 'react-router';
+import {withRouter} from 'react-router-dom';
 
 
 class Search extends React.Component {
@@ -33,7 +33,7 @@ class Search extends React.Component {
   }
 
   handleOK(){
-    this.props.changeSearchString(this.state.value);
+    // this.props.changeSearchString(this.state.value);
     this.props.history.push(`/search/${this.state.value}`);
   }
 
@@ -67,5 +67,5 @@ class Search extends React.Component {
     );
   }
 }
-
+// export default Search;
 export default withRouter(Search);
